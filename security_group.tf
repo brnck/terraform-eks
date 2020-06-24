081013bcc6b61d75b9b8fb8c9671cd961fb72e10
Augustas Berneckas <a.berneckas@gmail.com>
2020-06-24 19:47:04 +0300

    Dummy commit for testing

--- security_group.tf
+++ security_group.tf
@@ -10,3 +10,16 @@ resource "aws_security_group" "eks_cluster" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "test_atlantis" {
+  name        = "test_atlantis"
+  description = "For atlantis testing"
+  vpc_id      = aws_vpc.eks_cluster.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
